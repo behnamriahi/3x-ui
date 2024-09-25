c29f48982c0c347d80200b6555f6546f793e750a
Behnam Riahi <behnamriahi88@gmail.com>
2024-09-25 16:09:20 +0330

    disable validate lowercase

--- web/service/inbound.go
+++ web/service/inbound.go
@@ -2028,11 +2028,12 @@ func validateEmail(email string) (bool, error) {
 		return false, errors.New("email contains spaces, please remove them")
 	}
 
+	/*
 	if email != strings.ToLower(email) {
 		return false, errors.New("email contains uppercase letters, please convert to lowercase")
 	}
-
-	emailPattern := `^[a-z0-9@._-]+$`
+	*/
+	emailPattern := `^[a-zA-Z0-9@._-]+$`
 	if !regexp.MustCompile(emailPattern).MatchString(email) {
 		return false, errors.New("email contains invalid characters, please use only lowercase letters, digits, and @._-")
 	}
